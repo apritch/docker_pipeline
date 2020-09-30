e6f7adefaddbe61c1632818c2f1e1cedecb6cf5a
Ashley Pritchard <ashleyjpritchard91@gmail.com>
2020-09-30 14:27:20 +0100

    Incorporation of mapping script to snakemake pipeline

--- Snakefile
+++ Snakefile
@@ -1,2 +1,10 @@
 configfile: "config.yaml"
 
+rule bwa_map:
+	input:
+		"shared_data/genome.fa",
+		lambda wildcards: config["samples"][wildcards.sample]
+	output:
+		"shared_data/mapped_output/{sample}.sam"
+	script:
+		"scripts/bwa_map.py"
--- scripts/bwa_map.py
+++ scripts/bwa_map.py
@@ -1,15 +1,28 @@
 import subprocess
 
-#create a shared directory between the host and docker container and pull the container id from the command line output
-process = subprocess.run('docker run -dit -P --name bwa-test -v /home/ashley/training/docker_pipeline/data:/shared_data biocontainers/bwa:v0.7.17_cv1', shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
-container_id = str(process.stdout)[0:4]
+def map_reads():
 
-#create output folder with rwx permissions so host can edit
-subprocess.run('docker exec ' + container_id + " /bin/sh -c 'mkdir -m 777 /shared_data/mapped_output'", shell=True)
+	"""function for read mapping using bwa mem as part of Snakemake docker_pipeline
+	"""
 
-#run bwa
-subprocess.run('docker exec ' + container_id + " /bin/sh -c 'bwa mem /shared_data/genome.fa /shared_data/samples/A.fastq > /shared_data/mapped_output/A.sam'", shell=True)
+	#create a shared directory between the host and docker container and pull the container id from the command line output
+	process_share = subprocess.run('docker run -dit -P --name bwa-test -v /home/ashley/training/docker_pipeline/shared_data:/shared_data biocontainers/bwa:v0.7.17_cv1', shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+	container_id = str(process_share.stdout)[0:4]
 
-#kill and remove container
-subprocess.run('docker kill bwa-test', shell=True)
-subprocess.run('docker rm bwa-test', shell=True)
+	#create output folder with rwx permissions so host can edit
+	subprocess.run('docker exec ' + container_id + " /bin/sh -c 'mkdir -m 777 /shared_data/docker_mapped_output'", shell=True)
+
+	#run bwa
+	sam = str(snakemake.output).split("/")[-1]
+	command = "bwa mem /" + str(snakemake.input[0]) + ' /' + str(snakemake.input[1]) + " > /shared_data/docker_mapped_output/" + sam
+	subprocess.run('docker exec ' + container_id + " /bin/sh -c '" + command + "'", shell=True)
+
+	#kill and remove container
+	subprocess.run('docker kill bwa-test', shell=True)
+	subprocess.run('docker rm bwa-test', shell=True)
+
+	#copy data to snakemake output directory and remove temporary docker directory
+	subprocess.run('cp shared_data/docker_mapped_output/' + sam + ' ' + str(snakemake.output), shell=True)
+	subprocess.run('rm -rf shared_data/docker_mapped_output', shell=True)
+
+map_reads()
